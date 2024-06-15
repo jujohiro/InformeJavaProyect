cec6dc33cd478d6d7d1a77db3a6503e371433ed3
jujohiro <159204761+jujohiro@users.noreply.github.com>
2024-06-14 23:25:44 -0500

    estructuras for

---
+++ EstructurasFor.java
@@ -0,0 +1,17 @@
+package EstructuiraFor;
+
+
+public class EstructurasFor {
+
+    public static void main(String[] args) {
+         
+        int numero = 5;  
+
+        for (int i = 1; i <= 10; i++) {  // Inicializamos i en 1, el ciclo se ejecuta mientras i sea menor o igual a 10, incrementamos i en cada iteración
+            System.out.println(numero + " x " + i + " = " + (numero * i));  // Imprimimos la multiplicación del número por i
+        }
+    }
+}
+      
+    
+
---
+++ EstructurasFor3.java
@@ -0,0 +1,7 @@
+
+package EstructuiraFor;
+
+
+public class EstructurasFor3 {
+    
+}
